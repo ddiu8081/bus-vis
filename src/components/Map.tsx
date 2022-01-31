b9899eb4d80793bd970444a705084f06839b54f8
ddiu8081 <ddiu8081@gmail.com>
2022-01-31 18:21:08 +0800

    feat: load all line data

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -48,15 +48,52 @@ const Component = (props: Props) => {
   })
 
   const loadData = async () => {
-    const data_line = await ky.get('https://ddiu-10049571.cos.ap-shanghai.myqcloud.com/beijing_line_1643535095.json').json()
-    const lineLayer = new LineLayer()
-      .source(data_line)
-      .size(0.6)
-      .shape('line')
-      .color('#312B60')
-
-    scene.addLayer(lineLayer)
-    lineLayer.show()
+    const data_line: string[] = await ky.get('https://service-4a2rnaqt-1251746595.bj.apigw.tencentcs.com/data/line', {searchParams: {city: '1'}}).json()
+    // console.log(data_line)
+    if (data_line) {
+      const totalPath = []
+      for (let i = 0; i < data_line.length; i++) {
+        const line_str = data_line[i]
+        const points = line_str.split(';').map(p => p.split(','))
+        let last_blng = 0
+        let last_blat = 0
+        let path = []
+        for (let j = 0; j < points.length; j++) {
+          const point = points[j]
+          const blng = parseInt(point[0]) + last_blng
+          const blat = parseInt(point[1]) + last_blat
+          last_blng = blng
+          last_blat = blat
+          path.push([blng / 1000000, blat / 1000000])
+        }
+        totalPath.push(path)
+      }
+      console.log(totalPath)
+      const geoJsonData = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "type": "MultiLineString",
+              "coordinates": totalPath
+            }
+          }
+        ]
+      }
+      const lineLayer = new LineLayer({
+        blend: 'normal',
+      })
+        .source(geoJsonData)
+        .size(0.6)
+        .shape('line')
+        .color('#312e81')
+  
+      scene.addLayer(lineLayer)
+      lineLayer.show()
+    }
+    
   }
 
 
