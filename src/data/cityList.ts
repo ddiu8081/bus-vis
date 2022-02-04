5088506408f09766c43bef3218b3fe5df23facac
ddiu8081 <ddiu8081@gmail.com>
2022-02-04 09:18:47 +0800

    feat(data): add Guangzhou city

--- src/data/cityList.ts
+++ src/data/cityList.ts
@@ -9,11 +9,16 @@ const cityList : CityList = {
     name: '上海',
     location: [121.4, 31.2],
   },
+  guangzhou: {
+    id: 'guangzhou',
+    name: '广州',
+    location: [113.4, 23.1],
+  },
   nanjing: {
     id: 'nanjing',
     name: '南京',
     location: [118.8, 32.0],
-  }
+  },
 }
 
 export default cityList
